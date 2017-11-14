f1a569a333693aa285d197d27ab01a16044ef269
Andy Burke <andy.burke@mailbox.earth>
2017-11-13 21:13:32 -0800

    improve CLI (read from stdin, REPL)

--- cli.js
+++ cli.js
@@ -3,16 +3,50 @@
 
 const simplecoder = require( './index.js' );
 
-const operation = ( process.argv[ 2 ] || '' ).toLowerCase();
-const value = ( process.argv[ 3 ] || '' );
-switch( operation ) {
-    case 'decode':
-        console.log( '"' + simplecoder.decode( value.split( '-' ) ).toString() + '"' );
-        break;
-    case 'encode':
-        console.log( simplecoder.encode( new Buffer( value ) ).join( '-' ) );
-        break;
-    default:
-        console.error( `unknown operation: ${ operation }` );
-        break;
+function handle_operation( operation, value ) {
+    switch( operation ) {
+        case 'decode':
+            console.log( '"' + simplecoder.decode( value.split( '-' ) ).toString() + '"' );
+            break;
+        case 'encode':
+            console.log( simplecoder.encode( new Buffer( value ) ).join( '-' ) );
+            break;
+        default:
+            console.error( `unknown operation: ${ operation }` );
+            break;
+    }
+}
+
+if ( process.argv.length === 2 ) {
+    const readline = require( 'readline' );
+    const rl = readline.createInterface( {
+        input: process.stdin,
+        output: process.stdout
+    } );
+
+    rl.on( 'line', line => {
+        const [ operation, value ] = line.split( ' ', 2 );
+        handle_operation( operation, value );
+    } );
+}
+else if ( process.argv.length === 3 ) {
+    const operation = process.argv[ 2 ].toLowerCase();
+
+    const readline = require( 'readline' );
+    const rl = readline.createInterface( {
+        input: process.stdin,
+        output: process.stdout
+    } );
+
+    rl.on( 'line', line => {
+        handle_operation( operation, line );
+    } );
+}
+else if ( process.argv.length === 4 ) {
+    const operation = process.argv[ 2 ].toLowerCase();
+    const value = process.argv[ 3 ];
+    handle_operation( operation, value );
+}
+else {
+    console.error( 'Usage: simplecoder [<encode|decode> <value>]' );
 }
